2e1db8528f7f601a772be268908902394b8bb9e6
raffazizzi <raffaeleviglianti@gmail.com>
2020-02-24 12:08:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import CETEI from 'CETEIcean';
 
 function App() {
+  const teiDiv = useRef(null);
+
+  useEffect(() => {
+    async function getTEI() {
+      const ct = new CETEI();
+      
+      // Override default CETEI TEI behaviors.
+      ct.addBehavior('tei', 'teiHeader', undefined);
+  
+      const teiData = await ct.getHTML5(`example.xml`);
+  
+      // Add here other DOM operations as needed.
+      teiDiv.current.innerHTML = teiData.outerHTML;
+    }
+    getTEI();
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={teiDiv}>Loading...</div>
     </div>
   );
 }
